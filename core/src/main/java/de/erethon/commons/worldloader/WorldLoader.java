0327dca08614c8b52cac72e2f55f0135af6df004
Daniel Saukel <sa.da@unitybox.de>
2018-07-17 15:44:35 +0200

    Add 1.13 layer (untested)

--- core/src/main/java/de/erethon/commons/worldloader/WorldLoader.java
+++ core/src/main/java/de/erethon/commons/worldloader/WorldLoader.java
@@ -31,6 +31,9 @@ public class WorldLoader {
 
     static {
         switch (CompatibilityHandler.getInstance().getInternals()) {
+            case v1_13_R1:
+                internals = new v1_13_R1();
+                break;
             case v1_12_R1:
                 internals = new v1_12_R1();
                 break;
